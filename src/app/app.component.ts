57f1c374eed9a2166f6130b9f5b2e1689b8f5e9b
Attinder <Attinderkahlon@gmail.com>
2022-11-28 14:52:19 +1100

    binding in components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'learnAngular';
-  name = 'Attinder';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
